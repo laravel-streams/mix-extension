6d49a03a7f9c8f76fdc481555607011697a036f0
Robin Radic <rradic@hotmail.com>
2021-02-06 21:54:51 +0100

    remove compiled lib folder from git

--- lib/index.d.ts
+++
@@ -1,9 +0,0 @@
-import api from 'laravel-mix';
-import { LaravelStreamExtension, LaravelStreamExtensionOptions } from './LaravelStreamExtension';
-export { LaravelStreamExtensionOptions, };
-export default LaravelStreamExtension;
-declare global {
-    var mix: typeof api & {
-        streams(options: LaravelStreamExtensionOptions): any;
-    };
-}
